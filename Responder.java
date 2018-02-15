eefcc9107e527e0bff72417f663330b36c8f1340
Your Name <you@example.com>
2018-02-15 17:33:23 +0100

    metodo generateResponse mejorado devuelve respuesta preparada o aleatoria

--- Responder.java
+++ Responder.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.HashMap;
 import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 public class Responder
 {
     Random aleatorio;
+    HashMap<String, String> respuestasInput;
     ArrayList <String> respuestas;
     /**
      * Construct a Responder - nothing to do
@@ -17,21 +19,32 @@ public class Responder
     public Responder()
     {
         aleatorio = new Random();
+        respuestasInput = new HashMap<>();
         respuestas = new ArrayList<>();
         respuestas.add(" venga hasta luegoooo");
         respuestas.add(" muy interesante pero....");
         respuestas.add(" estoy ocupado, estoy con Ana");
         respuestas.add(" no me importa");
         respuestas.add(" hablamos mas tarde");
-        
+
+        respuestasInput.put("hola", "hola que tal");
+        respuestasInput.put("oferta", "lo siento no me interesa");
+        respuestasInput.put("viagra", "gracias pero funciono bien XD");
+        respuestasInput.put("importante", "me pondre en contacto contigo lo mas rapido posible");
     }
 
     /**
      * Generate a response.
      * @return   A string that should be displayed as the response
      */
-    public String generateResponse()
-    {        
-        return respuestas.get(aleatorio.nextInt(respuestas.size()));
+    public String generateResponse(String input)
+    {   
+        String respuesta = null;
+        respuesta = respuestasInput.get(input);
+        if(respuesta == null)
+        {
+            respuesta = respuestas.get(aleatorio.nextInt(respuestas.size()));
+        }      
+        return respuesta;
     }
 }
